ea89993a991b73e29efd91aef7b0f1e5b4b79509
jetal patel <145288109+j2patel4545@users.noreply.github.com>
2024-09-21 22:56:11 +0530

    UserModel.js

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    partnerName: { type: String, required: true }
+    partnerName: { type: String, required: true },
+    loveLuckPercentage: { type: Number, required: true } // Add loveLuckPercentage
 }, {
     timestamps: true // Adds createdAt and updatedAt fields
 });
